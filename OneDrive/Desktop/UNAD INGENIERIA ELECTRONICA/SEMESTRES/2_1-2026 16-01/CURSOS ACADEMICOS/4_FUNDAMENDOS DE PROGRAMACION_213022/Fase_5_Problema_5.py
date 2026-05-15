fae9df30208cd9924302a337a2b55599ec58f068
Pablo Elías Pérez Cardona <peperezca@unadvirtual.edu.co>
2026-05-14 23:23:18 -0500

    Pedir nombres y horas de cada recurso(con validación)

--- OneDrive/Desktop/UNAD INGENIERIA ELECTRONICA/SEMESTRES/2_1-2026 16-01/CURSOS ACADEMICOS/4_FUNDAMENDOS DE PROGRAMACION_213022/Fase_5_Problema_5.py
+++ OneDrive/Desktop/UNAD INGENIERIA ELECTRONICA/SEMESTRES/2_1-2026 16-01/CURSOS ACADEMICOS/4_FUNDAMENDOS DE PROGRAMACION_213022/Fase_5_Problema_5.py
@@ -8,3 +8,9 @@ print("Código Fuente:---------------- autoría propia")
 # R1: Solicitar al usuario el número de recursos
 num_recursos = int(input("Ingrese el número de recursos: "))      # Pide cuántos recursos se van a ingresar y convierte la entrada a entero
 equipo = []                                                       # Inicializa la matriz vacía donde se guardarán los recursos
+
+# R2: Pedir nombre y horas de cada recurso (con validación)
+for i in range(num_recursos):                                     # Recorre desde 0 hasta el número de recursos
+    nombre = input(f"Ingrese el nombre del recurso {i+1}: ")      # Solicita el nombre del recurso actual
+    horas = []                                                    # Lista vacía para almacenar las horas de lunes a viernes
+
